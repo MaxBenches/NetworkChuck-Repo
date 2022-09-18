f9c7053c30789e20134d60a25ef719765095083e
MaxBenchese <maxbech@gmail.com>
2022-09-18 18:10:07 +0200

    Added lists to code

---
+++ CampingList.py
@@ -0,0 +1,23 @@
+#Hey, we're going on a camping trip!
+
+#Here is the stuff we will need
+
+#tent, sleeping bags, water, raspberry pi, coffee, knie, ethernet cable, flash drive, beard oil, marshmallows
+
+camping_stuff = "tent, sleeping bags, water, raspberry pi, coffee, knie, ethernet cable, flash drive, beard oil, marshmallows"
+
+#print(camping_stuff)
+
+#Python List:
+
+camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+camp_site = ["Crystal Lake", 404, 89.3, True]
+
+me = camping_list[4]
+friend = camping_list[-2]
+
+print(me)
+print(friend)
+
+print(camping_list)
